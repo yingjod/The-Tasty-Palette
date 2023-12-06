456e8d9edcedb94c3cca6fa8649e7caa66ca90af
gcarnieli <gilbertocarnieli@hotmail.com>
2023-12-06 12:13:24 +0000

    "Recipe models and index"

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import 'dotenv/config'
+
+
+// It's our variable 
+const app = express()
+
+
+// Middleware
+app.use(express.json())
+
+// Logger 
+app.use((req, res, next) => {
+  console.log(`Request received: ${req.method} ${req.url}` )
+
+  next()
+})
+
+// End point 
+// app.use('/api', router)
+
+// Start our Server
+
+async function startServer(){
+  try {
+    await mongoose.connect(process.env.CONNECTION_STRING)
+    console.log('✅ Database connections established')
+    app.listen(process.env.PORT, () => console.log(`🦻🏽 Server listenig on port ${process.env.PORT}`))
+  } catch (error) {
+    console.log('🆘 Error establishing connection')
+    console.log(error)
+  }
+}
+startServer()
---
+++ models/recipe.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+
+const reviewSchema = new mongoose.Schema({
+  text: { type: String, required: true, maxlength: 300 },
+  rating: { type: Number, required: true, min: 1, max: 5 },
+  owner: { type: mongoose.ObjectId, ref: 'User', required: true },
+}, {
+  timestamps: true,
+})
+
+const recipeSchema = new mongoose.Schema({
+  title: { type: String, required: true, unique: true },
+  category: { type: String, required: true },
+  description: { type: String, required: true },
+  prepTime: { type: Number, required: true },
+  ingredients: [String],
+  poster: String,
+  method: { type: String, required: true },
+  rating: { type: Number },
+  review: [reviewSchema],
+  owner: { type: mongoose.ObjectId, ref: 'User', required: true },
+})
+
+export default mongoose.model('Recipe', recipeSchema)
