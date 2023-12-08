9b25dca941475a350cfa2518d27e68ae1af09c3c
lizalex7 <lizmannis7@gmail.com>
2023-12-08 18:11:52 +0000

    "Login page styled"

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,1 +1,27 @@
-export default function Login(){}
+import { useEffect } from 'react'
+import { Form, useActionData, useNavigate } from 'react-router-dom'
+import { setToken } from '../utilities/helpers/common'
+
+export default function Login(){
+  const res = useActionData()
+  const navigate = useNavigate()
+  
+  useEffect(() => {
+    if (res?.status === 202){
+      setToken(res.data.token)
+      navigate('/bread')
+    }
+  }, [res, navigate])
+
+  return (
+    <>
+      <h1 className="text-center bold display-3 mb-4">Login</h1>
+      <Form className='form' method="POST">
+        <input type="email" name="email" placeholder='Email' />
+        <input type="password" name="password" placeholder="Password" />
+        <button className='submit-btn' type="submit">Login</button>
+        {res && <p className='danger'>{res.data.message}</p>}
+      </Form>
+    </>
+  )
+}
---
+++ client/src/utilities/helpers/common.js
@@ -0,0 +1,38 @@
+const tokenName = 'SEI-76-BREADBORED-TOKEN'
+
+// This function takes a request object and returns form data as a JS object
+export async function formToObj(request){
+  const formData = await request.formData()
+  return Object.fromEntries(formData.entries())
+}
+
+export function setToken(token){
+  localStorage.setItem(tokenName, token)
+}
+
+export function getToken(){
+  return localStorage.getItem(tokenName)
+}
+
+export function removeToken(){
+  localStorage.removeItem(tokenName)
+}
+
+export function activeUser(){
+
+  const token = getToken()
+  if (!token) return null
+
+  // If token exists
+  const b64 = token.split('.')[1]
+  const payload = JSON.parse(atob(b64))
+
+  const now = Date.now() / 1000
+  const exp = payload.exp
+  if (exp > now) {
+    console.log(payload.sub)
+    return payload.sub
+  }
+
+  // Validate expiry date (payload.exp) by checking the number is greater than the date right now
+}
