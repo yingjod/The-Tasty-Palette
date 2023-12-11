554655d6be534e2bec87d17a4c7407a258e76347
gcarnieli <gilbertocarnieli@hotmail.com>
2023-12-11 15:59:06 +0000

    "last update"

--- client/src/components/SingleRecipe.jsx
+++ client/src/components/SingleRecipe.jsx
@@ -1,50 +1,146 @@
 
-import { useLoaderData } from "react-router-dom";
+// import { useLoaderData } from "react-router-dom";
+
+// export default function SingleRecipe() {
+//   const recipe = useLoaderData();
+//   const { title, description, prepTime, ingredients, method, poster, reviews } = recipe;
+
+//   console.log('Reviews:', reviews); // Add this line for debugging
+
+//   return (
+//     <>
+//       <div>
+//         <h2>{title}</h2>
+//         <img src={poster} alt={`Image of ${title}`} />
+//         <p>{description}</p>
+//         <p>{prepTime} mins</p>
+//         <p>{ingredients}</p>
+//         <p>{method}</p>
+        
+//         <h3>Reviews</h3>
+//         {reviews && reviews.length > 0 ? (
+//           <ul style={{ listStyle: 'none', padding: 0 }}>
+//             {reviews.map((review, index) => (
+//               <li key={index}>
+//                 {review.rating !== undefined && (
+//                   <div>
+//                     <p style={{ margin: 0, padding: 0 }}>
+//                       {/* Display stars based on the rating */}
+//                       {Array(review.rating).fill().map((_, starIndex) => (
+//                         <span key={starIndex}>★</span>
+//                       ))}
+//                       {/* Display the username who gave the rating */}
+//                       {review.owner && review.owner.username ? ` Rated by ${review.owner.username}` : 'Unknown User'}
+//                     </p>
+//                     {review.text && <p>{`Comment: ${review.text}`}</p>}
+//                   </div>
+//                 )}
+//               </li>
+//             ))}
+//           </ul>
+//         ) : (
+//           <p>No reviews yet.</p>
+//         )}
+//       </div>
+//     </>
+//   );
+// }
+
+import { useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { getToken } from '../utilities/helpers/common';
+import axios from 'axios';
 
 export default function SingleRecipe() {
-  const recipe = useLoaderData();
-  const { title, description, prepTime, ingredients, method, poster, reviews } = recipe;
+  const [recipe, setRecipe] = useState(useLoaderData());
+  const [formData, setFormData] = useState({ rating: 5, text: '' });
 
-  console.log('Reviews:', reviews); // Add this line for debugging
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmitReview = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(`/api/recipes/${recipe._id}/reviews`, formData, {
+        headers: {
+          Authorization: `Bearer ${getToken()}`,
+        },
+      });
+      console.log('Review created:', response.data);
+      setFormData({ rating: 5, text: '' });
+      // Fetch updated recipe data after creating the review
+      // You may need to modify the endpoint or add additional logic based on your server setup
+      const updatedRecipe = await axios.get(`/api/recipes/${recipe._id}`);
+      // Update the component state with the latest recipe data
+      // This assumes that the server returns the entire recipe including reviews
+      setRecipe(updatedRecipe.data);
+    } catch (error) {
+      console.error('Error creating review:', error);
+    }
+  };
 
   return (
     <>
       <div>
-        <h2>{title}</h2>
-        <img src={poster} alt={`Image of ${title}`} />
-        <p>{description}</p>
-        <p>{prepTime} mins</p>
-        <p>{ingredients}</p>
-        <p>{method}</p>
+        <h2>{recipe.title}</h2>
+        <img src={recipe.poster} alt={`Image of ${recipe.title}`} />
+        <p>{recipe.description}</p>
+        <p>{recipe.prepTime} mins</p>
+        <p>{recipe.ingredients}</p>
+        <p>{recipe.method}</p>
 
         <h3>Reviews</h3>
-        {reviews && reviews.length > 0 ? (
+        {recipe.reviews && recipe.reviews.length > 0 ? (
           <ul style={{ listStyle: 'none', padding: 0 }}>
-            {reviews.map((review, index) => (
+            {recipe.reviews.map((review, index) => (
               <li key={index}>
-                {review.rating !== undefined && (
-                  <div>
-                    <p style={{ margin: 0, padding: 0 }}>
-                      {/* Display stars based on the rating */}
-                      {Array(review.rating).fill().map((_, starIndex) => (
-                        <span key={starIndex}>★</span>
-                      ))}
-                      {/* Display the username who gave the rating */}
-                      {review.owner && review.owner.username ? ` Rated by ${review.owner.username}` : 'Unknown User'}
-                    </p>
-                    {review.text && <p>{`Comment: ${review.text}`}</p>}
-                  </div>
-                )}
+                <p style={{ margin: 0, padding: 0 }}>
+                  {Array(review.rating).fill().map((_, starIndex) => (
+                    <span key={starIndex}>★</span>
+                  ))}
+                  {review.owner && review.owner.username ? ` Rated by ${review.owner.username}` : 'Unknown User'}
+                </p>
+                {review.text && <p>{`Comment: ${review.text}`}</p>}
               </li>
             ))}
           </ul>
         ) : (
           <p>No reviews yet.</p>
         )}
+
+        <form onSubmit={handleSubmitReview}>
+          <label>
+            Rating:
+            <input
+              type="number"
+              name="rating"
+              value={formData.rating}
+              onChange={handleInputChange}
+              min="1"
+              max="5"
+            />
+          </label>
+          <label>
+            Comment:
+            <textarea
+              name="text"
+              value={formData.text}
+              onChange={handleInputChange}
+            />
+          </label>
+          <button type="submit">Submit Review</button>
+        </form>
       </div>
     </>
   );
 }
+
+
+
+
+
+
 
 
 
--- client/src/utilities/actions/auth.js
+++ client/src/utilities/actions/auth.js
@@ -1,25 +1,73 @@
-import axios from "axios"
+// import axios from "axios"
 
 
+
+// export async function registerUser(request) {
+//   const data = await formToObj(request)
+//   return await axios.post('/api/register', data, {
+//     validateStatus: () => true
+//   })
+
+// }
+
+
+
+// export async function loginUser(request){
+//   const data = await formToObj(request)
+//   return await axios.post('/api/login', data, {
+//     validateStatus: () => true
+//   })
+// }
+
+// async function formToObj(request) {
+//   const formData = await request.formData()
+//   return Object.fromEntries(formData.entries())
+// }
+
+import axios from "axios";
 
 export async function registerUser(request) {
-  const data = await formToObj(request)
-  return await axios.post('/api/register', data, {
-    validateStatus: () => true
-  })
+  try {
+    const data = await formToObj(request);
+    const response = await axios.post('/api/register', data, {
+      validateStatus: () => true
+    });
 
+    if (response.status === 201) {
+      return response.data.token;
+    } else {
+      // Handle registration failure, e.g., show an error message
+      console.error('Registration failed:', response.data);
+      return null;
+    }
+  } catch (error) {
+    console.error('Error during registration:', error);
+    return null;
+  }
 }
 
+export async function loginUser(request) {
+  try {
+    const data = await formToObj(request);
+    const response = await axios.post('/api/login', data, {
+      validateStatus: () => true
+    });
 
-
-export async function loginUser(request){
-  const data = await formToObj(request)
-  return await axios.post('/api/login', data, {
-    validateStatus: () => true
-  })
+    if (response.status === 202) {
+      return response.data.token;
+    } else {
+      // Handle login failure, e.g., show an error message
+      console.error('Login failed:', response.data);
+      return null;
+    }
+  } catch (error) {
+    console.error('Error during login:', error);
+    return null;
+  }
 }
 
 async function formToObj(request) {
-  const formData = await request.formData()
-  return Object.fromEntries(formData.entries())
+  const formData = await request.formData();
+  return Object.fromEntries(formData.entries());
 }
+
